624ee264b90f32662f960d853c055cbedcaa35ac
lightlogic <frederic.noyer@vd.ch>
2020-12-26 19:50:41 +0100

    backend routing refactored

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
+const featuresRoutes = require('./routes/features');
+
 const env = "development";
 const config = require("./secrets.js")[env];
-
-const Feature = require("./models/feature");
 
 const app = express();
 
@@ -48,37 +48,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/features", (req, res, next) => {
-  // req.body is a new object added to the request by body-parser
-  const feature = new Feature({
-    //id: req.body.id,
-    uri: req.body.uri,
-    description: req.body.description,
-    wktGeometry: req.body.wktGeometry,
-    projection: req.body.projection,
-  });
-  feature.save().then((createdFeature) => {
-    res.status(201).json({
-      message: "Feature added sucessfully",
-      featureId: createdFeature._id,
-    });
-  });
-});
+// from ./routes/features.js (see import on top)
+// in app.use, the "/api/features" serves as a filter.
+// only the routes starting with /api/features will be
+// forwared in ./routes/features.js
+app.use("/api/features", featuresRoutes);
 
-app.get("/api/features", (req, res, next) => {
-  Feature.find().then((documents) => {
-    res.status(200).json({
-      message: "Features fetched successfully !",
-      features: documents,
-    });
-  });
-});
 
-app.delete("/api/features/:id", (req, res, next) => {
-  Feature.deleteOne({ _id: req.params.id }).then((result) => {
-    console.log(result);
-    res.status(200).json({ message: "Post deleted" });
-  });
-});
 
 module.exports = app;
